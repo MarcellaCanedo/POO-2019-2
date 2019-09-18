3fb1d878bf5aba6d8190f988cc8b80686ebcb238
MarcellaCanedo <marcellacanedo@hotmail.com>
2019-09-18 10:09:05 -0300

    CREATE - Garagem MVC

---
+++ Garagem (v1.0)/Garagem/src/Marca.java
@@ -0,0 +1,9 @@
+public class Marca {
+
+    private String nome_Marca;
+
+    public String getNome_Marca() { return nome_Marca; }
+
+    public void setNome_Marca(String nome_Marca) { this.nome_Marca = nome_Marca; }
+
+}
